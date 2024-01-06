1eb50f551226931c38bd3946e8070f76cd541265
Marcus Ribeiro <marcus10dev@gmail.com>
2024-01-06 16:01:56 -0300

    Header

--- portifolio/src/components/Header/Header.js
+++ portifolio/src/components/Header/Header.js
@@ -1,13 +1,29 @@
 import React from 'react'
 
 //CSS
-import style from './Header.module.css'
+import './Header.css'
+
+//Images
+import brand_logo from '../../images/brand_logo.png'
 
 const Header = () => {
   return (
-    <header className={style.headerContainer}>
-        <h1>Esse é o cabeçalho</h1>
-    </header>
+    <>
+      <header className='header-component'>
+        <nav className='row bg-secondary'>
+          <div className='col d-flex align-content-center justify-content-start'>
+            <a href='...'><img src={brand_logo} alt='imagem que leva a home' className='img-styles' /></a>
+          </div>
+          <div className='col d-flex align-content-center justify-content-center '>
+            <ul className='list-unstyled d-flex'>
+                <li><a href='...'>Me Conheça</a></li>
+                <li><a href='...'>Competências</a></li>
+                <li><a href='...'>Experiência</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
   )
 }
 
