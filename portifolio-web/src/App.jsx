5c0476b2a159ec8d5c1d314d96c5a64584076956
Marcus Ribeiro <marcus10dev@gmail.com>
2024-01-17 20:13:27 -0300

    Alterando layout componente Presentation

--- portifolio-web/src/App.jsx
+++ portifolio-web/src/App.jsx
@@ -4,11 +4,19 @@ import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects/Projects';
 
 function App() {
+  const isJsDisabled = typeof window === 'undefined' || !('ontouchstart' in window);
+
+  function jsDisabled() {
+    alert('Por favor ative o javascript na página ou use um navegador mais moderno');
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Presentation />
       <Projects />
+
+      {isJsDisabled && <noscript>{jsDisabled}</noscript>}
     </div>
   );
 }
--- portifolio-web/src/components/Presentation/Presentation.jsx
+++ portifolio-web/src/components/Presentation/Presentation.jsx
@@ -2,18 +2,45 @@ import './Presentation.css';
 
 //Images
 import brand_logo from '../../assets/images/brand_logo.png';
+import { useState } from 'react';
 
 const Presentation = () => {
+  const [languageVideo, setLanguageVideo] = useState(true);
+
   return (
     <main className="Apresentation">
       <div className="containerWidth">
-        <div className="container-main">
-          <p className="">
-            Marcus aqui <span className="animationHi">✌️</span> Saudações
-          </p>
-          <span>Desenvolvedor</span>
-          <div className="d-flex container-subtitles">
+        <div className="row">
+          <div className="col-7 d-flex flex-column">
+            <p className="">
+              Marcus aqui <span className="animationHi">✌️</span> Saudações
+            </p>
+            <span>Desenvolvedor</span>
             <span>Full-Stack</span>
+          </div>
+          <div className="col d-flex justify-content-center align-items-center flex-column ">
+            <div>
+              <button className="btn btn-info" onClick={() => setLanguageVideo(true)}>
+                PT/BR
+              </button>
+              <button className="btn btn-danger" onClick={() => setLanguageVideo(false)}>
+                EN/US
+              </button>
+            </div>
+            {languageVideo && (
+              <video controls>
+                <source src="seu_video.mp4" type="video/mp4" />
+                Seu navegador não suporta o elemento de vídeo.
+              </video>
+            )}
+            {!languageVideo && (
+              <video controls>
+                <source src="seu_video1.mp4" type="video/mp4" />
+                Seu navegador não suporta o elemento de vídeo.
+              </video>
+            )}
+          </div>
+          {/* <div className="d-flex container-subtitles">
             <div className="container-images">
               <a href="...">
                 <img src={brand_logo} alt="Link do github" />
@@ -25,11 +52,7 @@ const Presentation = () => {
                 <img src={brand_logo} alt="Link do currículo" />
               </a>
             </div>
-          </div>
-          <p>
-            Estudo o desenvolvimento front-end e back-end a cerca de 1 ano e meio
-            <br /> estou no meu 5º semestre da graduação
-          </p>
+          </div> */}
         </div>
       </div>
     </main>
