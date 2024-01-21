3c01b92d07984c43cb70d0fadfd1294dfe100243
Marcus Ribeiro <marcus10dev@gmail.com>
2024-01-21 13:29:07 -0300

    Responsividade do componente Presentation

--- portifolio-web/src/components/Presentation/Presentation.jsx
+++ portifolio-web/src/components/Presentation/Presentation.jsx
@@ -5,6 +5,12 @@ import './Presentation.css';
 import { useState, useEffect } from 'react';
 
 const Presentation = () => {
+  const [isTrue, setIsTrue] = useState(false);
+
+  const toggleState = () => {
+    setIsTrue(!isTrue);
+  };
+
   const [languageVideo, setLanguageVideo] = useState(true);
 
   const [isMobile, setIsMobile] = useState(false);
@@ -27,24 +33,34 @@ const Presentation = () => {
 
   return (
     <main className="Apresentation">
+      <div className="language-container">
+        <p>Inglês</p>
+        <div className={`toggle-container ${isTrue ? 'true' : ''}`} onClick={toggleState}>
+          <div className="toggle-ball"></div>
+        </div>
+      </div>
       <div className="containerWidth">
-        <div className="row">
-          <div className="col-7 d-flex flex-column">
+        <div
+          className={
+            !isMobile
+              ? 'd-flex justify-content-around'
+              : 'd-flex flex-column justify-content-between align-items-center '
+          }
+        >
+          <div className="d-flex flex-column justify-content-center ">
             <p className="">
               Marcus aqui <span className="animationHi">✌️</span> Saudações
             </p>
-            <span>Desenvolvedor</span>
             <span>Full-Stack</span>
+            <span>Developer</span>
           </div>
-          <div className="col d-flex justify-content-center align-items-center flex-column ">
-            <div>
-              <button className="btn btn-info" onClick={() => setLanguageVideo(true)}>
-                PT/BR
-              </button>
-              <button className="btn btn-danger" onClick={() => setLanguageVideo(false)}>
-                EN/US
-              </button>
-            </div>
+          <div className="div-video d-flex flex-column justify-content-center">
+            {/* <button className="btn btn-info" onClick={() => setLanguageVideo(true)}>
+              PT/BR
+            </button>
+            <button className="btn btn-danger" onClick={() => setLanguageVideo(false)}>
+              EN/US
+            </button> */}
             {languageVideo && (
               <video controls>
                 <source src="seu_video.mp4" type="video/mp4" />
